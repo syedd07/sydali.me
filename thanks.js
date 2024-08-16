fdcd88e9b1869d9a162205d631787b8746dfa5e0
Syed Yousuf Ali <93760528+syedd07@users.noreply.github.com>
2024-08-16 15:18:55 +0530

    Delete thanks.js

--- thanks.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        document.getElementById('loader').classList.add('hidden');
-        document.getElementById('thank-you-container').classList.remove('hidden');
-    }, 2000); // Simulating a 3-second load time
-});
