9100b5e49787058c17775c77ad4621657df463ef
Syed Yousuf Ali <syedyusufali6626@gmail.com>
2024-09-08 20:13:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,13 +44,7 @@ const Nav = props => {
 
     React.createElement("span", null))))));
 
-
-
-
-
 };
-
-
 
 /***********************
   Header Component
@@ -120,7 +114,7 @@ const About = props => {
     React.createElement("p", { className: "separator" })), /*#__PURE__*/
     React.createElement("div", { className: "desc full" }, /*#__PURE__*/
     React.createElement("div", { className: "course-link", style: { textAlign: "center" } },
-    React.createElement("a", { href: "/dist/certificates/index.html", onClick: props.toggleMenu, style: { fontStyle: "italic", textDecoration: "underline" } }, "Course Certificate ", " ", /*#__PURE__*/
+    React.createElement("a", { href: "/certificates/index.html", onClick: props.toggleMenu, style: { fontStyle: "italic", textDecoration: "underline" } }, "Course Certificate ", " ", /*#__PURE__*/
     React.createElement("i", { className: "fas fa-external-link-alt" })),
     React.createElement("p",{ClassName: "separator"}),
     React.createElement("p", null, "I have completed my different courses from online education perovider like coursera and scrima.. etc; you can see my certificates by clicking on the link below.")))), /*#__PURE__*/
